bf372ada473058f9180500a4d6825a5ddaa7a73c
Daniela Colque Chura <colque_daniela@hotmail.com>
2025-10-02 10:10:26 -0300

    Borrado de codigo csfr

--- mvc_cementerio/app/controllers/DifuntoController.php
+++ mvc_cementerio/app/controllers/DifuntoController.php
@@ -448,12 +448,5 @@ class DifuntoController extends Control
         exit;
     }
 
-    private function generateCsrfToken() {
-        if (!isset($_SESSION['csrf_token'])) {
-            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-        }
-        return $_SESSION['csrf_token'];
-    }
-
 }
 ?>
